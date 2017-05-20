b1224fa4b5d505ddd31d0bb024337d298e490b86
e <598105852@qq.com>
2017-05-21 01:25:43 +0800

    add 小红点

--- app/src/main/java/com/guokm/tibetanroot/adapter/MessageItemAdapter.java
+++ app/src/main/java/com/guokm/tibetanroot/adapter/MessageItemAdapter.java
@@ -1,14 +1,18 @@
 package com.guokm.tibetanroot.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.guokm.tibetanroot.R;
 import com.guokm.tibetanroot.domain.MessageItem;
+import com.jauker.widget.BadgeView;
 
 import java.util.List;
 
@@ -17,16 +21,18 @@ import java.util.List;
  */
 
 public class MessageItemAdapter extends BaseAdapter {
-    private static final String TAG="MessageItemAdapter";
+    private static final String TAG = "MessageItemAdapter";
     private List<MessageItem> itemList;
     Context context;
-    public MessageItemAdapter(Context context,List itemList){
-        this.itemList=itemList;
-        this.context=context;
+
+    public MessageItemAdapter(Context context, List itemList) {
+        this.itemList = itemList;
+        this.context = context;
     }
+
     @Override
     public int getCount() {
-        return itemList==null?0:itemList.size();
+        return itemList == null ? 0 : itemList.size();
     }
 
     @Override
@@ -44,22 +50,40 @@ public class MessageItemAdapter extends BaseAdapter {
         MessageItem messageItem = (MessageItem) getItem(i);
         View view;
         ViewHolder viewHolder;
-        if(convertView==null){
-            view = LayoutInflater.from(context).inflate(R.layout.item_message,parent,false);
-            viewHolder=new ViewHolder();
-            viewHolder.title_tv= (TextView) view.findViewById(R.id.title_message_tv);
-            viewHolder.time_tv= (TextView) view.findViewById(R.id.time_message_tv);
+        if (convertView == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.item_message, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.title_tv = (TextView) view.findViewById(R.id.title_message_tv);
+            viewHolder.time_tv = (TextView) view.findViewById(R.id.time_message_tv);
+            viewHolder.message_item_layout = (RelativeLayout) view.findViewById(R.id.message_item_layout);
             view.setTag(viewHolder);
-        }else {
-            view=convertView;
-            viewHolder= (ViewHolder) view.getTag();
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
         }
         viewHolder.title_tv.setText(messageItem.getTitle());
         viewHolder.time_tv.setText(messageItem.getDate());
+
+
+        //设置badgeview显示，和判断是否消失
+        BadgeView badgeView = new BadgeView(context);
+//        if (看过了 == ture) {
+            badgeView.setBadgeCount(0);//设置为0则控件红点消失
+//        } else {
+            badgeView.setTargetView(viewHolder.message_item_layout);
+//        badgeView.setBackgroundColor(Color.BLUE);
+            badgeView.setBackground(7, Color.RED);
+//        badgeView.setBadgeCount(1);
+            badgeView.setText("未读");
+            badgeView.setBadgeGravity(Gravity.CENTER_VERTICAL | Gravity.RIGHT);
+//        }
         return view;
     }
-    class ViewHolder{
+
+    static class ViewHolder {
         TextView title_tv;
         TextView time_tv;
+        RelativeLayout message_item_layout;
+
     }
 }
